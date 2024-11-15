8c10b7e64b463b07e958373514106abb7f4aec18
kateyeh <kate850209@gmail.com>
2024-11-15 23:30:25 +0800

    Initial Commit

---
+++ HW1_Start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1_Start
+//
+//  Created by Kateyeh on 2024/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
